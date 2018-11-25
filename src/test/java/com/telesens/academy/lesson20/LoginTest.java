a2ca13f0409b73309f0d51cf2d6a7a279e9ed76b
oleksii.kormilets <oleksii,kav@gmail.com>
2018-11-25 14:12:45 +0200

    Data provider get test Data from XLS-file

--- src/test/java/com/telesens/academy/lesson20/LoginTest.java
+++ src/test/java/com/telesens/academy/lesson20/LoginTest.java
@@ -2,7 +2,11 @@ package com.telesens.academy.lesson20;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 
+import com.telesens.academy.lesson17.PropertyDemo;
+import com.telesens.academy.loginform.ReadFromFileXLS;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
@@ -16,26 +20,39 @@ public class LoginTest {
         private boolean acceptNextAlert = true;
         private StringBuffer verificationErrors = new StringBuffer();
 
+    @Parameters("browser")
     @BeforeClass(alwaysRun = true)
-    public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "D:/Local/chromedriver.exe");
-        //System.setProperty("webdriver.gecko.driver", "D:/Local/geckodriver.exe");
-        driver = new ChromeDriver();
-        //driver = new FirefoxDriver();
-        baseUrl = "https://www.katalon.com/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    public void setUp(@Optional("chrome") String browser) throws Exception {
+        switch (browser) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "D:/Local/chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
 
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "D:/Local/geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+
+            default:
+                throw new UnsupportedOperationException("Not supported browser: " + browser);
+        }
+
+        baseUrl = PropertyDemo.readProperty("urladdress");//"http://store.demoqa.com"
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
-        @Test
-        public void testCorrectLoginTestCase() throws Exception {
-            driver.get("http://store.demoqa.com/");
+
+    @Test(dataProvider = "positiveLoginProvider", groups = "LoginTrue")
+        public void testCorrectLoginTestCase(String login, String password) throws Exception {
+            String strurl=PropertyDemo.readProperty("urladdress");
+            driver.get(strurl);
             driver.findElement(By.id("account")).click();
             driver.findElement(By.id("log")).click();
             driver.findElement(By.id("log")).clear();
-            driver.findElement(By.id("log")).sendKeys("KAV123g");
+            driver.findElement(By.id("log")).sendKeys(login);
             driver.findElement(By.id("pwd")).clear();
-            driver.findElement(By.id("pwd")).sendKeys("Passw0rd!");
+            driver.findElement(By.id("pwd")).sendKeys(password);
             driver.findElement(By.id("login")).click();
 
             Assert.assertNotEquals(driver.findElement(By.linkText("(Logout)")), null) ;
@@ -44,24 +61,32 @@ public class LoginTest {
             driver.close();
         }
 
-    @Test
-    public void IncorrectLoginTestCase() throws Exception { //if input correct - test failed
-        driver.get("http://store.demoqa.com/");
+    @Test(dataProvider = "negativeLoginProvider", groups = "LoginFalse")
+    public void IncorrectLoginTestCase(String login, String password, String errMessage) throws Exception { //if input correct - test failed
+        String strurl=PropertyDemo.readProperty("urladdress");
+        driver.get(strurl);
         driver.findElement(By.id("account")).click();
         driver.findElement(By.id("log")).click();
         driver.findElement(By.id("log")).clear();
-        driver.findElement(By.id("log")).sendKeys("KAV123");
+        driver.findElement(By.id("log")).sendKeys(login);
         driver.findElement(By.id("pwd")).click();
         driver.findElement(By.id("pwd")).clear();
-        driver.findElement(By.id("pwd")).sendKeys("Passw0rd!1");
+        driver.findElement(By.id("pwd")).sendKeys(password);
         driver.findElement(By.id("login")).click();
-        //String Error_Show = driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Your Account'])[1]/following::strong[1]")).getText();
-        Assert.assertEquals(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Your Account'])[1]/following::strong[1]")).getText(), "ERROR");
+
+        String errorShow = driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Your Account'])[1]/following::strong[1]")).getText();
+        //Assert.assertEquals(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Your Account'])[1]/following::strong[1]")).getText(), "ERROR");
         /*try {
             assertEquals(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Your Account'])[1]/following::strong[1]")).getText(), "ERROR");
         } catch (Error e) {
             verificationErrors.append(e.toString());
         }*/
+        //Thread.sleep(3000); // bad practice
+        //WebElement message = (new WebDriverWait(driver, 7))
+         //       .until(ExpectedConditions
+          //              .presenceOfElementLocated(By.className("response")));
+        Assert.assertEquals(errorShow,errMessage);
+
         driver.close();
     }
 
@@ -74,6 +99,28 @@ public class LoginTest {
                 fail(verificationErrorString);
             }
         }
+
+    @DataProvider(name="negativeLoginProvider")
+    public Object[][] negativeLoginProvider() {
+        Object newobj[][]=null;
+        newobj=ReadFromFileXLS.readFromXLX(2,3,3);
+        return newobj;
+     /*   return new Object[][]{
+               // {"KAV123xc", "Passw0rd!1", "ERROR: Invalid username. Lost your password?"},
+                {"KAV123", "Passw0rd!", "ERROR: Invalid username. Lost your password?"}
+        };*/
+    }
+
+    @DataProvider(name="positiveLoginProvider")
+    public Object[][] positiveLoginProvider() {
+        Object newobj[][]=null;
+        newobj=ReadFromFileXLS.readFromXLX(1,1,2);
+        return newobj;
+     /*   return new Object[][]{
+               // {"KAV123xc", "Passw0rd!1", "ERROR: Invalid username. Lost your password?"},
+                {"KAV123", "Passw0rd!", "ERROR: Invalid username. Lost your password?"}
+        };*/
+    }
 
         private boolean isElementPresent(By by) {
             try {
---
+++ src/test/java/com/telesens/academy/loginform/ReadFromFileXLS.java
@@ -0,0 +1,65 @@
+package com.telesens.academy.loginform;
+
+import com.telesens.academy.lesson17.PropertyDemo;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ReadFromFileXLS {
+    public static String[][] readFromXLX (int startRowNumber, int endRowNumber, int cellsCount) {
+        XSSFCell cell;
+
+        File file = new File(PropertyDemo.readProperty("loginfile"));
+        String[][] objarray = new String[endRowNumber - startRowNumber + 1][cellsCount];
+
+        try (XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(file))) {
+            XSSFSheet sheet = workbook.getSheet("Sheet1");
+            int objindex=0;
+            for (int r = startRowNumber - 1; r < endRowNumber; r++) {
+                XSSFRow row = sheet.getRow(r);
+                if (row != null) {
+                    for (int c = 0; c < cellsCount; c++) {
+                        cell = row.getCell(c);
+                        if (cell != null) {
+                            switch (cell.getCellType()) {
+                                case XSSFCell.CELL_TYPE_FORMULA:
+                                    objarray[objindex][c] = cell.getCellFormula();
+                                    break;
+                                case XSSFCell.CELL_TYPE_NUMERIC:
+                                    objarray[objindex][c] = ""
+                                            + cell.getNumericCellValue();
+                                    break;
+                                case XSSFCell.CELL_TYPE_STRING:
+                                    objarray[objindex][c] = ""
+                                            + cell.getStringCellValue();
+                                    break;
+                                case XSSFCell.CELL_TYPE_BLANK:
+                                    objarray[objindex][c] = "[BLANK]";
+                                    break;
+                                case XSSFCell.CELL_TYPE_ERROR:
+                                    objarray[objindex][c] = "" + cell.getErrorCellValue();
+                                    break;
+                                default:
+                            }
+                            //System.out.print(objarray[r][c]);
+                        } else {
+                            objarray[objindex][c] = "";
+                        }
+                    } // for(c)
+                }// if row!=null
+                objindex++;
+            }
+
+            return objarray;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
